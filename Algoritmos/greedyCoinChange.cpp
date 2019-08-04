abe9124b24fcac67c28d3b81ad2c9eb02e2a31ce
Lalo Rivero <hola@lalorivero.com>
2019-08-04 12:16:44 -0600

    Algoritmo greedy, se agregaron comentarios

--- Algoritmos/greedyCoinChange.cpp
+++ Algoritmos/greedyCoinChange.cpp
@@ -7,18 +7,18 @@ funcion recursiva para encontrar el valor y retornar "monedas necesarias para el
 #include <climits>
 using namespace std;
 
-int greedyChange(int coinSet[], int n, int C){
-    if(C==0){
+int greedyChange(int coinSet[], int n, int Cambio){
+    if(Cambio==0){ //Si no tenemos que dar cambio detiene la ejecucion 
         return 0;
-    }if(C<0){
+    }if(Cambio<0)
+    { // Si las operaciones resultan en numeros negativos regresa  INT_MAX -> Maximum value for an object of type int | 32767 (215-1) or greater* | <climits>
         return INT_MAX;
     }
     int coins = INT_MAX;
 
     ///Recorremos todo nuestro set de monedas AKA opciones para dar cambio
     for(int i = 0 ; i < n ; i++){
-        int res = greedyChange(coinSet, n, C - coinSet[i]);
-       
+        int res = greedyChange(coinSet, n, Cambio - coinSet[i]);
         if( res != INT_MAX){
             coins = min(coins, res + 1);
         }
@@ -27,8 +27,8 @@ int greedyChange(int coinSet[], int n, int C){
 }
 
 int main (){
-    int coinSet[]={1,5,10,15,20};
-    int n = sizeof(coinSet)/sizeof(coinSet[0]);
+    int coinSet[]={1,5,10,15,20}; // son las monedas con las que cuento para dar cambio 
+    int n = sizeof(coinSet)/sizeof(coinSet[0]); // Es el tamaño del array
     cout<<"Ingresa cuando es tu cambio "<<endl;
     int cambio = 0;
     cin>>cambio;
